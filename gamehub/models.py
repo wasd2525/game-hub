566833c4c0e1afe7a66ea8a7876eb4f4ae936667
wasd2525 <hben123@gmail.com>
2017-02-26 20:11:37 +0200

    res fixes

--- gamehub/models.py
+++ gamehub/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    image = models.ImageField(upload_to = 'static/img',blank=True, null=True)
+    image = models.ImageField(upload_to = 'game-hub/gamehub/static/img',blank=True, null=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
